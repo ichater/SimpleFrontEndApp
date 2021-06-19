f539b9b59f3fdb38a4420a109dc5b6c22ff37c64
ichater <chaterizaak@gmail.com>
2021-06-19 15:33:20 +1000

    added everything else

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 const heading = document.querySelector(".heading-main");
 
-heading.addEventListener("click", function () {
-  alert("Yes we do!!");
+heading.addEventListener("click", ()=> {
+  console.log("this is a console log")
 });
+
+
+const increment = document.querySelector(".increment");
+const numberToChange = document.querySelector(".number-to-change");
+const decrement = document.querySelector(".decrement");
+
+increment.addEventListener("click", ()=>{
+  numberToChange.textContent = parseInt(numberToChange.textContent) + 1
+})
+
+decrement.addEventListener("click", function(){
+  numberToChange.textContent = parseInt(numberToChange.textContent) - 1
+})
+
+const h2Wrapper = document.querySelector(".h2-wrapper")
+const h2Green = document.querySelector(".green-h2")
+const h2Red = document.querySelector(".red-h2")
+
+h2Green.addEventListener("click", function(){
+  h2Green.classList.remove('green-h2');
+h2Green.classList.add('red-h2');
+})
+
+h2Red.addEventListener("click", function(){
+  h2Red.classList.remove('red-h2');
+  h2Red.classList.add('green-h2');
+})
